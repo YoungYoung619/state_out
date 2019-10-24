ac396078dc0667810b6933888af28a03b2d26c53
YoungYoung619 <940752885@qq.com>
2019-10-24 17:52:29 +0800

    'situation_assess'

--- situation_assessment.py
+++ situation_assessment.py
@@ -99,6 +99,33 @@ def gaussian_1d(x, mean, for_what, std, max):
 
     return score
 
+
+def comfort_level_scores(acc):
+    """ assess the comfort level
+    Args:
+        acc: the acc val for host vehicle
+    """
+    def norm(x, mu, sigma):
+        """normal gaussian function
+        """
+        pdf = math.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
+        return pdf
+
+    def gaussian_1d(x, sigma=5):
+        if x >= 0:
+            return np.array([0., 0., 1.])
+        else:
+            x = math.fabs(x)
+            # print(x)
+            if x >= MAX_DECELERATION:
+                return np.array([1, norm(x, AVG_DECELERATION, 6), norm(x, COMFORTABLE_DECELERATION, 1.5)])
+            elif x <= COMFORTABLE_DECELERATION:
+                return np.array([norm(x, MAX_DECELERATION, 6), norm(x, AVG_DECELERATION, 1.5), 1])
+            else:
+                return np.array([norm(x, MAX_DECELERATION, 6), norm(x, AVG_DECELERATION, 1.5), norm(x, COMFORTABLE_DECELERATION, 1.5)])
+
+    return gaussian_1d(acc)
+
 def _score_2_threat_degree(score):
     if np.argmax(score) == 0:
         return safety_degree.dangerous
